488813bf8802db3cb343c33a6f9a012ce6f8735c
Everton Fernandes <evertonfernandes.jsx@gmail.com>
2023-02-08 16:06:57 -0300

    feat: :sparkles: profile photo upload

---
+++ src/screens/Profile.tsx
@@ -0,0 +1,120 @@
+import { Button } from '@components/Button'
+import { Input } from '@components/Input'
+import { ScreenHeader } from '@components/ScreenHeader'
+import { UserPhoto } from '@components/UserPhoto'
+import * as FileSystem from 'expo-file-system'
+import * as ImagePicker from 'expo-image-picker'
+import {
+  Center,
+  Heading,
+  KeyboardAvoidingView,
+  Skeleton,
+  Text,
+  useToast,
+  VStack,
+} from 'native-base'
+import { useState } from 'react'
+import { ScrollView, TouchableOpacity } from 'react-native'
+
+const PHOTO_SIZE = 33
+
+export function Profile() {
+  const [photoIsloading, setPhotoIsloading] = useState(false)
+  const [userPhoto, setUserPhoto] = useState(
+    'https://github.com/evertonFS7.png',
+  )
+
+  const toast = useToast()
+
+  async function handleUserPhotoSelect() {
+    setPhotoIsloading(true)
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
+
+      if (photoSelected.canceled) return
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri,
+        )
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500',
+          })
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setPhotoIsloading(false)
+    }
+  }
+
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView contentContainerStyle={{ paddingBottom: 56 }}>
+        <KeyboardAvoidingView behavior="position" enabled>
+          <Center mt={6} px={10}>
+            {photoIsloading ? (
+              <Skeleton
+                w={PHOTO_SIZE}
+                h={PHOTO_SIZE}
+                rounded="full"
+                startColor="gray.500"
+                endColor="gray.400"
+              />
+            ) : (
+              <UserPhoto
+                source={{ uri: userPhoto }}
+                alt="Foto do usuário"
+                size={PHOTO_SIZE}
+              />
+            )}
+            <TouchableOpacity onPress={handleUserPhotoSelect}>
+              <Text
+                color="green.500"
+                fontWeight="bold"
+                fontSize="md"
+                mt={2}
+                mb={8}
+              >
+                Alterar foto
+              </Text>
+            </TouchableOpacity>
+            <Input bg="gray.600" placeholder="Nome" />
+            <Input bg="gray.600" placeholder="E-mail" isDisabled />
+
+            <Heading
+              color="gray.200"
+              fontSize="md"
+              mb={2}
+              alignSelf="baseline"
+              mt={12}
+            >
+              Alterar senha
+            </Heading>
+            <Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
+            <Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
+            <Input
+              bg="gray.600"
+              placeholder="Confirme a nova senha"
+              secureTextEntry
+            />
+            <Button title="Atualizar" mt={4} />
+          </Center>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </VStack>
+  )
+}
