804647b84dec26302f97c1b189ec9228e76def19
Everton Fernandes <evertonfernandes.jsx@gmail.com>
2023-02-08 15:58:28 -0300

    feat: :sparkles: rendering routes

--- App.tsx
+++ App.tsx
@@ -1,22 +1,28 @@
-import { Loading } from '@components/Loading';
-import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { NativeBaseProvider } from 'native-base';
-import { StatusBar, View } from 'react-native';
+import { Loading } from '@components/Loading'
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+import { NativeBaseProvider } from 'native-base'
+import { StatusBar } from 'react-native'
+
 import { THEME } from './src/theme'
+import { Routes } from '@routes/index'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
+    Roboto_700Bold,
   })
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
-        barStyle='light-content'
-        backgroundColor='transparent'
+        barStyle="light-content"
+        backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Loading /> : <View />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
-  );
+  )
 }
