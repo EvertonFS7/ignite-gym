b103d154b385848735be76b9d85dc5dff6571c7b
Everton Fernandes <evertonfernandes.jsx@gmail.com>
2023-02-09 14:42:07 -0300

    feat: :sparkles: form validation

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,68 +1,128 @@
 import LogoSvg from '@assets/logo.svg'
 import { Button } from '@components/Button'
 import { Input } from '@components/Input'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { useNavigation } from '@react-navigation/native'
-import BackgroundImg from 'assets/background.png'
-import { Center, Heading, Image, Text, VStack } from 'native-base'
-import { Platform, ScrollView } from 'react-native'
 import { AuthNavigatorRoutesProps } from '@routes/auth.routes'
+import BackgroundImg from 'assets/background.png'
+import {
+  Center,
+  Heading,
+  Image,
+  KeyboardAvoidingView,
+  Text,
+  VStack,
+} from 'native-base'
+import { Controller, useForm } from 'react-hook-form'
+import { Platform, ScrollView } from 'react-native'
+import * as yup from 'yup'
+
+type FormDataProps = {
+  email: string
+  password: string
+}
+
+const signInSchema = yup.object({
+  email: yup.string().required('Informe o seu email').email('E-mail inválido.'),
+  password: yup
+    .string()
+    .required('Informe a sua senha')
+    .min(6, 'A senha deve conter no mínimo 6 caracteres.'),
+})
 
 export function SignIn() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    resolver: yupResolver(signInSchema),
+  })
+
   const { navigate } = useNavigation<AuthNavigatorRoutesProps>()
+
+  function handleSignIn(data: FormDataProps) {
+    console.log(data)
+  }
 
   function handleNewAccount() {
     navigate('SignUp')
   }
 
   return (
-    <ScrollView
-      contentContainerStyle={{ flexGrow: 1 }}
-      showsVerticalScrollIndicator={false}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      enabled
+      flex={1}
     >
-      <VStack flex={1} px={10} pb={Platform.OS === 'ios' ? 40 : 16}>
-        <Image
-          source={BackgroundImg}
-          defaultSource={BackgroundImg}
-          alt="Pessoas treinando"
-          resizeMode="contain"
-          position="absolute"
-        />
-
-        <Center my={24}>
-          <LogoSvg />
-
-          <Text color="gray.100" fontSize="sm">
-            Treine sua mente e o seu corpo
-          </Text>
-        </Center>
-
-        <Center>
-          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-            Acesse sua conta
-          </Heading>
-
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <VStack px={10}>
+          <Image
+            source={BackgroundImg}
+            defaultSource={BackgroundImg}
+            alt="Pessoas treinando"
+            resizeMode="contain"
+            position="absolute"
           />
-          <Input placeholder="Senha" secureTextEntry />
 
-          <Button title="Acessar" />
-        </Center>
+          <Center my={24}>
+            <LogoSvg />
 
-        <Center mt={24}>
-          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
-            Ainda não tem acesso?
-          </Text>
+            <Text color="gray.100" fontSize="sm">
+              Treine sua mente e o seu corpo
+            </Text>
+          </Center>
 
-          <Button
-            title="Criar conta"
-            variant="outline"
-            onPress={handleNewAccount}
-          />
-        </Center>
-      </VStack>
-    </ScrollView>
+          <Center>
+            <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+              Acesse sua conta
+            </Heading>
+
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="E-mail"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.email?.message}
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Senha"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.password?.message}
+                />
+              )}
+            />
+
+            <Button title="Acessar" onPress={handleSubmit(handleSignIn)} />
+          </Center>
+
+          <Center mt={24}>
+            <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+              Ainda não tem acesso?
+            </Text>
+
+            <Button
+              title="Criar conta"
+              variant="outline"
+              onPress={handleNewAccount}
+            />
+          </Center>
+        </VStack>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -3,107 +3,154 @@ import { Button } from '@components/Button'
 import { Input } from '@components/Input'
 import { useNavigation } from '@react-navigation/native'
 import BackgroundImg from 'assets/background.png'
-import { Center, Heading, Image, Text, VStack } from 'native-base'
+import {
+  Center,
+  Heading,
+  Image,
+  KeyboardAvoidingView,
+  Text,
+  VStack,
+} from 'native-base'
 import { Controller, useForm } from 'react-hook-form'
 import { Platform, ScrollView } from 'react-native'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+type FormDataProps = {
+  name: string
+  email: string
+  password: string
+  password_confirm: string
+}
+
+const signUpSchema = yup.object({
+  name: yup.string().required('informe o nome.'),
+  email: yup.string().required('Informe o e-mail.').email('E-mail inválido.'),
+  password: yup
+    .string()
+    .required('Informe a senha.')
+    .min(6, 'A senha deve ter no mínimo 6 dígitos.'),
+  password_confirm: yup
+    .string()
+    .required('Confirme a senha.')
+    .oneOf([yup.ref('password')], 'A confirmação da senha não confere.'),
+})
 
 export function SignUp() {
-  const { control, handleSubmit } = useForm()
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema),
+  })
+
   const { goBack } = useNavigation()
 
   function handleGoBack() {
     goBack()
   }
 
-  function handleSignUp(data: any) {
+  function handleSignUp(data: FormDataProps) {
     console.log(data)
   }
   return (
-    <ScrollView
-      contentContainerStyle={{ flexGrow: 1 }}
-      showsVerticalScrollIndicator={false}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      enabled
+      flex={1}
     >
-      <VStack flex={1} px={10} pb={Platform.OS === 'ios' ? 40 : 16}>
-        <Image
-          source={BackgroundImg}
-          defaultSource={BackgroundImg}
-          alt="Pessoas treinando"
-          resizeMode="contain"
-          position="absolute"
-        />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <VStack px={10}>
+          <Image
+            source={BackgroundImg}
+            defaultSource={BackgroundImg}
+            alt="Pessoas treinando"
+            resizeMode="contain"
+            position="absolute"
+          />
 
-        <Center my={24}>
-          <LogoSvg />
+          <Center my={24}>
+            <LogoSvg />
 
-          <Text color="gray.100" fontSize="sm">
-            Treine sua mente e o seu corpo
-          </Text>
-        </Center>
+            <Text color="gray.100" fontSize="sm">
+              Treine sua mente e o seu corpo
+            </Text>
+          </Center>
 
-        <Center>
-          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-            Crie sua conta
-          </Heading>
-          <Controller
-            control={control}
-            name="name"
-            render={({ field: { onChange, value } }) => (
-              <Input placeholder="Nome" onChangeText={onChange} value={value} />
-            )}
-          />
-          <Controller
-            control={control}
-            name="email"
-            render={({ field: { onChange, value } }) => (
-              <Input
-                placeholder="E-mail"
-                keyboardType="email-address"
-                autoCapitalize="none"
-                onChangeText={onChange}
-                value={value}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="password"
-            render={({ field: { onChange, value } }) => (
-              <Input
-                placeholder="Senha"
-                secureTextEntry
-                onChangeText={onChange}
-                value={value}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="password_confirm"
-            render={({ field: { onChange, value } }) => (
-              <Input
-                placeholder="Confirme a Senha"
-                secureTextEntry
-                onChangeText={onChange}
-                value={value}
-                onSubmitEditing={handleSubmit(handleSignUp)}
-                returnKeyType="send"
-              />
-            )}
-          />
+          <Center>
+            <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+              Crie sua conta
+            </Heading>
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Nome"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.name?.message}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="E-mail"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.email?.message}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Senha"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.password?.message}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="password_confirm"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Confirme a Senha"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  value={value}
+                  onSubmitEditing={handleSubmit(handleSignUp)}
+                  returnKeyType="send"
+                  errorMessage={errors.password_confirm?.message}
+                />
+              )}
+            />
+
+            <Button
+              title="Criar e acessar"
+              onPress={handleSubmit(handleSignUp)}
+            />
+          </Center>
 
           <Button
-            title="Criar e acessar"
-            onPress={handleSubmit(handleSignUp)}
+            mt={12}
+            title="Voltar para o login"
+            variant="outline"
+            onPress={handleGoBack}
           />
-        </Center>
-
-        <Button
-          mt={24}
-          title="Voltar para o login"
-          variant="outline"
-          onPress={handleGoBack}
-        />
-      </VStack>
-    </ScrollView>
+        </VStack>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
