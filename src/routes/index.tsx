3dbf34dc589f38f7bb2cd0514d1ce108f910279e
Everton Fernandes <evertonfernandes.jsx@gmail.com>
2023-02-16 17:20:27 -0300

    using hook useAuth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
+import { useAuth } from '@hooks/useAuth'
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
-import { useTheme, Box } from 'native-base'
+import { Box, useTheme } from 'native-base'
 
-import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
   const { colors } = useTheme()
+
+  const { user } = useAuth()
+
+  console.log('Usuário logado =>', user)
+
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
 
